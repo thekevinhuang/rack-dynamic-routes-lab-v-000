199052eb81d74997e67f6183d4e93f294d9ee585
Kevin Huang <yellow.khaos@gmail.com>
2018-02-21 04:46:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,17 @@
 class Application
+  
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
 
 
     resp.finish
